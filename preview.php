aef48c5e788108cd35d1fd2015471508fafc33d8
Dangofinetime <dangofinetime@mail.ru>
2018-09-06 19:53:15 +0300

    'MyFile'

--- preview.php
+++ preview.php
@@ -1,1 +1,10 @@
-<!-- Это мой файл. (Я Даня)... -->
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Danny's</title>
+</head>
+<body>
+    <h1>Это мой файлик.</h1>
+</body>
+</html>
